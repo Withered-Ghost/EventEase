5f765a26f6661e8fb9b2ea486e2c3cdf1c796e39
kaushikjaincodes <kaushikjain2626@gmail.com>
2024-11-03 21:01:12 +0530

    Cors and Few changes

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -9,9 +9,10 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 chatRouter.post('/send', async (req, res) => {
-    const prompt = req.body.msg;
+    const prompt = req.body.message;
     try {
         const result = await model.generateContent(prompt);
+        // console.log(result.response.text())
         res.status(200).json({
             reply: result.response.text(),
         });
--- server.js
+++ server.js
@@ -2,16 +2,19 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import {chatRouter} from "./routes/chat.route.js"
+import cors from 'cors'
 dotenv.config({path: './.env'});
-
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 app.use(express.json());
 
 // app.use('/api/user', userRouter);
 // app.use('/api/event', eventRouter);
 // app.use('/api/task', taskRouter);
+
 app.use('/api/chat', chatRouter);
+
 async function main() {
     await mongoose.connect(process.env.MONGODB_URI + process.env.MONGODB_EventEase);
     app.listen(port, () => console.log('Server listening on port: ' + port));
