10e45ae1a9d9525507c046f2e3ab81f41d4ecdaa
Withered-Ghost <62442.smit@gmail.com>
2024-11-03 18:42:41 +0530

    update server.js, add routes

--- server.js
+++ server.js
@@ -1,40 +1,21 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import mongoose from "mongoose";
-import UserModel from "./models/user.model.js";
-import EventModel from "./models/event.model.js";
-import TaskModel from "./models/task.model.js";
+import mongoose from 'mongoose';
 
 dotenv.config({path: './.env'});
 
-mongoose.connection.on('connected', () => console.log('connected'));
-mongoose.connection.on('open', () => console.log('open'));
-mongoose.connection.on('disconnected', () => console.log('disconnected'));
-mongoose.connection.on('reconnected', () => console.log('reconnected'));
-mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
-mongoose.connection.on('close', () => console.log('close'));
-
 const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.json());
 
-async function main() {
-    mongoose.connect(process.env.MONGODB_URI + process.env.MONGODB_EventEase)
-    .then(
-        async function() {
-            const newUser = new UserModel({
-                email: 'nigga',
-                password: 'pass',
-                name: 'naaame'
-            });
+// app.use('/api/user', userRouter);
+// app.use('/api/event', eventRouter);
+// app.use('/api/task', taskRouter);
+// app.use('/api/chat', chatRouter);
 
-            await UserModel.create(newUser)
-            .then(() => console.log('new user inserted'))
-            .catch((err) => console.log(err));
-        }
-    )
-    .catch((err) => {
-        console.log(err);
-    });
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URI + process.env.MONGODB_EventEase);
+    app.listen(port, () => console.log('Server listening on port: ' + port));
 }
 
 main();
