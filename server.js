d4b009213ac6d9c86c125cf6f60fdb3bea5d769b
kaushikjaincodes <kaushikjain2626@gmail.com>
2024-11-04 15:37:06 +0530

    Jwt return payload as user._id

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ import eventRouter from './routes/event.router.js';
 import { chatRouter } from './routes/chat.router.js';
 import cors from 'cors';
 
-dotenv.config({path: './.env'});
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
+// app.use('/',(req,res)=>{
+//     res.json({
+//         msg : "Yes the backend is working"
+//     })
+// })
 app.use('/api/user', userRouter);
 app.use('/api/event', eventRouter);
 // app.use('/api/task', taskRouter);
